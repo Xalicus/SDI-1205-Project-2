3acd5e87d67172321e8dfc1f405053b2b1b2d37e
Kevin Ward <Xalicus@fullsail.edu>
2012-05-10 18:31:39 -0700

    Added the Say command for all my code, to save space.

--- Ward_Kevin_Assignment2.js
+++ Ward_Kevin_Assignment2.js
@@ -1,6 +1,6 @@
 // Author: Kevin Ward
-// Date: May 3rd, 2012
-// Name: Deliverable 1
+// Date: May 7rd, 2012
+// Name: Deliverable 2
 // Theme: Procrastination and Schoolwork.
 
 
@@ -8,39 +8,50 @@
 
 var longTime = true, reallyHungry = true, cheeseSlices = "cheese", hoursLeft = 2;
 
+// Say function for space saving code!
+var say = function(message) { console.log(message); };
+
 // My Procrastination Adventure!
 
-console.log("It\’s 10am, time to get on to my schoolwork for the week.");
-console.log("I don\’t really feel up to it, so I make a decision to play my game console instead.");
+say("It\’s 10am, time to get on to my schoolwork for the week.");
+say("I don\’t really feel up to it, so I make a decision to play my game console instead.");
 if (longTime === true) {
-	console.log("I play for a couple hours until 3pm.");
+	say("I play for a couple hours until 3pm.");
 } else {
-	console.log("I play for a few hours until 12pm.");
+	say("I play for a few hours until 12pm.");
 };
-console.log("Crap, it\’s really three o\’clock, I should get started on school.");
-console.log("But, it can wait a couple more hours, so I decide to get something to eat.");
-console.log("Oh but what to eat?");
-console.log("I have pasta or hamburgers, hmmm.");
+say("Crap, it\’s really three o\’clock, I should get started on school.");
+say("But, it can wait a couple more hours, so I decide to get something to eat.");
+say("Oh but what to eat?");
+say("I have pasta or hamburgers, hmmm.");
 if (reallyHungry === true) {
-	console.log("Let\’s go with the hamburgers!");
+	say("Let\’s go with the hamburgers!");
 } else {
-	console.log("Pasta sounds really yummy now!");
+	say("Pasta sounds really yummy now!");
 };
-console.log("So I start grilling up some nice, juicy hamburgers in the kitchen.");
-console.log("Now what should I put on the buns?");
-console.log("There\’s mayo, mustard, relish, BBQ sauce, ketchup, cheese, etc.");
+say("So I start grilling up some nice, juicy hamburgers in the kitchen.");
+say("Now what should I put on the buns?");
+say("There\’s mayo, mustard, relish, BBQ sauce, ketchup, cheese, etc.");
 if (cheeseSlices === "cheese") {
-	console.log("I think I\’ll just put on some mayo, mustard, BBQ sauce, and cheese slices.");
+	say("I think I\’ll just put on some mayo, mustard, BBQ sauce, and cheese slices.");
 } else {
-	console.log("I\'ll just put on ketchup, mustard, relish, and cheese slices, instead.");
+	say("I\'ll just put on ketchup, mustard, relish, and cheese slices, instead.");
 };
-console.log("I eat up all the yumminess, and finally decide I\’m ready for the week\’s schoolwork, but now I only have a couple hours left in the day until it\’s due.");
-console.log("What should I do?");
-console.log("Cram as much as I can in the hours I have left, or procrastinate until tomorrow?");
+say("I eat up all the yumminess, and finally decide I\’m ready for the week\’s schoolwork, but now I only have a couple hours left in the day until it\’s due.");
+say("What should I do?");
+say("Cram as much as I can in the hours I have left, or procrastinate until tomorrow?");
 if (hoursLeft >= 3) {
-	console.log("I can cram it all in tonight!");
+	say("I can cram it all in tonight!");
 } else {
-	console.log("Ooh Noes!!! I have to wait until tomorrow to finish.");
+	say("Ooh Noes!!! I have to wait until tomorrow to finish.");
 };
-console.log("Number 2 sounds really good to me.");
-console.log("The End!");
+say("Number 2 sounds really good to me.");
+say("The End!");
+
+
+        
+        
+        
+        
+        
+        
